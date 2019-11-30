9a4921491505317d66897b0244c9fbc544f404c4
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-11-30 12:07:05 +0530

    Frequency Counter-w.SingleLoop O(n)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JavaScript-DataStructures-Algorithms
+
+  ## Big O Notation
+  ### It allows us to talk or to express how exactly the runtime of an algorithm grows as th input grows. we need not care a lot about the details, as this is only a basic nderstanding course. 
+
+  ### We say that an algorith of O(f(n)) if the number of simple operations the computer has to do is eventually less than a constant times f(n), as n increases.
---
+++ Section 1/frequencyCounter.js
@@ -0,0 +1,18 @@
+// Write a function which accepts two arrays and returns true if the other array is a squared array to the prior.
+
+//  arr1        arr2     return 
+//[1,2,-3]    [1,4,9]     true
+
+checkSq = (arr1, arr2) => {
+    if(arr1.length!==arr2.length){
+        return 'Match not found.'
+    }
+    for(let i = 0; i <= arr1.length; i++){
+        var correctIndex = arr2.indexOf(arr1[i] ** 2);
+        if(correctIndex === -1){
+            return 'Match Not found.'
+        } arr2.splice(correctIndex, 1)
+    } return true
+}
+
+console.log(checkSq([1,2,3,2], [1,4,9,4]));
