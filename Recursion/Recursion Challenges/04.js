83b2afd97b164aadc66395105a463c963115a059
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-12-06 14:41:27 +0530

    challenge #04 add the numbers

---
+++ Recursion/Recursion Challenges/04.js
@@ -0,0 +1,12 @@
+
+// Write a function called recursiveRange which accepts a number 
+// and adds up all the numbers from 0 to the number passed to the function 
+
+recursiveRange = (number) => {
+    if(number===0){
+        return 0
+    }
+    return number+recursiveRange(number-1)
+}
+
+console.log(recursiveRange(5));
