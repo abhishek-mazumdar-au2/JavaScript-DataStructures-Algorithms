4ab1416de3e0afc87feed318e3e32f6d29f7d6f9
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-12-07 14:15:57 +0530

    Selection Sort again

--- Recursion/Sorting/Selection-Sort/01.js
+++ Recursion/Sorting/Selection-Sort/01.js
@@ -1,29 +1,59 @@
-selectionSort = (arr) => {
-    var min, noSwap;
+// selectionSort = (arr) => {
+//     var min, noSwap;
 
-    for(var i = 0; i < arr.length; i++){
-        let min = i;
-        noSwap = true;
-        for(var j = i+1; j < arr.length; j++){
-            console.log(i, j);
-            if( arr[j] < arr[min] ){
-                // change min
-                min = j;
-                noSwap = false;
-            }
+//     for(var i = 0; i < arr.length; i++){
+//         let min = i;
+//         noSwap = true;
+//         for(var j = i+1; j < arr.length; j++){
+//             console.log(i, j);
+//             if( arr[j] < arr[min] ){
+//                 // change min
+//                 min = j;
+//                 noSwap = false;
+//             }
+//         }
+//         // SWAP
+//         const temp = arr[i];
+//         arr[i] = arr[min];
+//         arr[min] = temp
+//         if (noSwap) {
+//             break;
+//         }
+//     }
+//     return arr
+// }
+// //               m
+// // [ 1 , 2 , 3 , 3 , 7 ] 
+// //               i   j
+// console.log(selectionSort( [7,3,2,3,1,10,39,8,2,1,3,-3,32,2] ));
+              
+
+    selectionSort = (arr) => {
+        let swap = false;
+            count = 0;
+for(var i = 0; i<arr.length; i++){
+    let m = i;
+    for(var j = i+1; j<arr.length; j++){
+        if(arr[j]<arr[m]){
+            m = j;
+            count ++
+            swap = true;
+            console.log(arr);
         }
-        // SWAP
-        const temp = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp
-        if (noSwap) {
-            break;
+        if(swap===false){
+            break
         }
     }
-    return arr
+    // SWAP
+    const temp = arr[i];
+    arr[i] = arr[m];
+    arr[m] = temp;
+    
 }
-//               m
-// [ 1 , 2 , 3 , 3 , 7 ] 
-//               i   j
-console.log(selectionSort( [7,3,2,3,1,10,39,8,2,1,3,-3,32,2] ));
-              
+    return ({arr, count})
+
+}
+console.log(selectionSort([ -3 , 3 , 2 , 3 , 1 , 10 , 39 , 8 , 2 , 1 , 3 , 7 , 32 ,2 ]))
+//        m
+// [ -3 , 3 , 2 , 3 , 1 , 10 , 39 , 8 , 2 , 1 , 3 , 7 , 32 ,2 ]
+//        i   j
