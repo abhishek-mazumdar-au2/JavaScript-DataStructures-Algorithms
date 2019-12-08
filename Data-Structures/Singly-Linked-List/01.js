7bf35e8ed63b41f9069b1a2d29947cd508fba597
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-12-09 02:58:31 +0530

    Singly-Linked-List pop()

--- Data-Structures/Singly-Linked-List/01.js
+++ Data-Structures/Singly-Linked-List/01.js
@@ -28,17 +28,56 @@ class SinglyLinkedList{
         this.length++
         return this;
     }
+
+    // .....TRAVERSE......
+    traverse(){
+        var current = this.head;
+        while(current){
+            console.log(current.val);
+            current = current.next;
+        }
+    }
+    // .......POP.........
+    pop(){
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--
+        return current
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 var list = new SinglyLinkedList();
 console.log(list);
-list.push('hello')
-list.push('everyone');
-list.push('everyONE');
-list.push('!');
-console.log(list.head.next);
-
-var first =  new Node('Hi');
-first.next = new Node('there');
-first.next.next = new Node('how');
-first.next.next.next = new Node('are');
-first.next.next.next.next = new Node('you?')
+list.push(1)
+list.push(2);
+list.push(3);
+list.push(4);
+console.log(list);
+console.log(list.pop());
+list.push(5);
+console.log(list);
+list.pop()
+console.log(list);
+// var first =  new Node('Hi');
+// first.next = new Node('there');
+// first.next.next = new Node('how');
+// first.next.next.next = new Node('are');
+// first.next.next.next.next = new Node('you?')
