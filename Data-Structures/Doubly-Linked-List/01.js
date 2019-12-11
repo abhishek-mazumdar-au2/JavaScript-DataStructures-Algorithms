2aea0af5b793b386542ade3afa3a45e3cee8cf05
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-12-11 16:07:45 +0530

    DLL push()

--- Data-Structures/Doubly-Linked-List/01.js
+++ Data-Structures/Doubly-Linked-List/01.js
@@ -12,11 +12,31 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    // -------PUSH-------
+    push(val){
+        var newNode = new Node(val);
+        if(this.length===0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            const prevTail = this.tail;
+            this.tail.next = newNode;
+            newNode.prev = prevTail;
+            this.tail = newNode;
+        }
+        this.length++
+        return this
+    }
 }
+// var list = new DoublyLinkedList();
+// var first = new Node(1);
+// console.log(first)
+// first.next = new Node(2)
+// console.log(first);
+// first.next.prev = first;
+// console.log(first.next);
+
 var list = new DoublyLinkedList();
-var first = new Node(1);
-console.log(first)
-first.next = new Node(2)
-console.log(first);
-first.next.prev = first;
-console.log(first.next);
+console.log(list);
+console.log(list.push(1))
+console.log(list.push(2))
