b84ae1bbbfe7d50522d6f7df340a41446adf4626
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-12-11 13:39:24 +0530

    Sngly-Linked-List reverse()

--- Data-Structures/Singly-Linked-List/01.js
+++ Data-Structures/Singly-Linked-List/01.js
@@ -140,6 +140,23 @@ class SinglyLinkedList{
         this.length--;
         return true;
     }
+
+    // -------reverse--------
+    reverse(){
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+        for(var i = 0 ; i < this.length; i++){
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+
 }
 //    pv    
 //    1-------2----4-----6
